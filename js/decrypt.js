93502415cd4b42398a665886d5e5070ee9652979
Arthur Guiot <arguiot@gmail.com>
2018-03-24 18:12:28 +0800

    Same for decrypt

--- js/decrypt.js
+++ js/decrypt.js
@@ -4,15 +4,17 @@ Use: "RIJVSUYVJN".decrypt("key")
 => "HELLOWORLD"
 
 **********************************/
-function ordA(a) {
-    return a.charCodeAt(0) - 65;
-}
-String.prototype.decrypt = function (key) {
+
+String.prototype.decrypt = function(key) {
+	function ordA(a) {
+		return a.charCodeAt(0) - 65;
+	}
 	let i = 0;
-    let b;
-    key = key.toUpperCase().replace(/[^A-Z]/g, '');
-    return this.toUpperCase().replace(/[^A-Z]/g, '').replace(/[A-Z]/g, a => {
-        b = key[i++ % key.length];
-        return String.fromCharCode(((ordA(a) + 26 - ordA(b)) % 26 + 65));
-    });
+	let b;
+	key = key.toUpperCase().replace(/[^A-Z]/g, '');
+	return this.toUpperCase().replace(/[^A-Z]/g, '').replace(/[A-Z]/g, a => {
+		b = key[i++ % key.length];
+		return String.fromCharCode(((ordA(a) + 26 - ordA(b)) % 26 + 65));
+	});
 }
+module.exports = (text, key) => text.decrypt(key)
