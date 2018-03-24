0c3261d058496c9f8f6f70670de5b6138aa64f5e
Arthur Guiot <arguiot@gmail.com>
2018-03-24 18:11:23 +0800

    Updated encrypt for compatibility with website

--- js/encrypt.js
+++ js/encrypt.js
@@ -4,15 +4,16 @@ Use: "Hello World".encrypt("key")
 => "RIJVSUYVJN"
 
 ******************************/
-function ordA(a) {
-    return a.charCodeAt(0) - 65;
-}
-String.prototype.encrypt = function (key) {
+String.prototype.encrypt = function(key) {
+	function ordA(a) {
+		return a.charCodeAt(0) - 65;
+	}
 	let i = 0;
-    let b;
-    key = key.toUpperCase().replace(/[^A-Z]/g, '');
-    return this.toUpperCase().replace(/[^A-Z]/g, '').replace(/[A-Z]/g, a => {
-        b = key[i++ % key.length];
-        return String.fromCharCode(((ordA(a) + ordA(b)) % 26 + 65));
-    });
+	let b;
+	key = key.toUpperCase().replace(/[^A-Z]/g, '');
+	return this.toUpperCase().replace(/[^A-Z]/g, '').replace(/[A-Z]/g, a => {
+		b = key[i++ % key.length];
+		return String.fromCharCode(((ordA(a) + ordA(b)) % 26 + 65));
+	});
 }
+module.exports = (text, key) => text.encrypt(key)
